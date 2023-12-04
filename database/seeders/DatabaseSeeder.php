3730957089bcb1ac6dc4ee2ab25bde145950f977
Caio <bladellano@gmail.com>
2023-12-04 19:02:55 +0000

    :hammer:Atualizando seeder com a criacao de videos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,5 +19,30 @@ class DatabaseSeeder extends Seeder
             'email' => 'bladellano@gmail.com',
             'password' => Hash::make('12345678'),
         ]);
+
+        // Created multi videos
+        \App\Models\Video::factory()->create([
+            'title' => 'Curso de Secretaria Escolar ',
+            'slug' => 'cillum-picanha-adipisicing-beef',
+            'description' => 'Curso de Secretaria Escolar',
+            'url' => 'https://www.youtube.com/embed/tyOysfVac20?si=_gFYjXL-dJxdKIXc',
+            'user_id' => 1,
+        ]);
+
+        \App\Models\Video::factory()->create([
+            'title' => 'O Papel da Escola',
+            'slug' => 'o-papel-da-escola',
+            'description' => 'O Papel da Escola',
+            'url' => 'https://www.youtube.com/embed/ZQ3WciCeAR0?si=hVOeng3qhXSeeW73',
+            'user_id' => 1,
+        ]);
+
+        \App\Models\Video::factory()->create([
+            'title' => 'CURSO SECRETARIA ESCOLAR',
+            'slug' => 'curso-secretaria-escolar',
+            'description' => 'CURSO SECRETARIA ESCOLAR ',
+            'url' => 'https://www.youtube.com/embed/pl2poFzP8v0?si=Mf9jvSgOhF99yefb',
+            'user_id' => 1,
+        ]);
     }
 }
